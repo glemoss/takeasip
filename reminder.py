0cdc6f0ac5ca93db4d47b690fc36d44b342e0664
glemoss <glemoss@yahoo.com>
2024-02-22 11:19:22 -0300

    remove deprecated dependencies

--- reminder.py
+++ reminder.py
@@ -1,13 +1,10 @@
 import time
 import os
 
-from dotenv import load_dotenv
 from mac_notifications import client
 
 
 if __name__ == "__main__":
-    load_dotenv()
-
     image_path=''
     seconds = 1800
 
