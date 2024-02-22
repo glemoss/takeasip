d476f5d8b8d78d7a4f9873ecd8ee733063cc584f
glemoss <glemoss@yahoo.com>
2024-02-22 11:16:39 -0300

    refactor

--- README.md
+++ README.md
@@ -1,13 +1,2 @@
 # takeasip
 Simple python script for notifying to drink water.
-
-### 1. Create a .env File
-
-In the root directory of your project, create a file named `.env`. This is where you'll define your environment variables.
-
-### 2. Add Environment Variables
-
-Inside the `.env` file, list your environment variables in the `KEY=VALUE` format. For example:
-
-```env
-IMAGE_PATH=/home/user/project/image
--- reminder.py
+++ reminder.py
@@ -8,11 +8,14 @@ from mac_notifications import client
 if __name__ == "__main__":
     load_dotenv()
 
+    image_path=''
+    seconds = 1800
+
     while True:
-        if time.time() % 1800 == 0:
+        if time.time() % seconds == 0:
             client.create_notification(
             title="Water Reminder",
             subtitle="Take a sip now!",
-            icon=os.environ["IMAGE_PATH"],
+            icon=os.environ[image_path],
             action_button_str="Drink",
 )
